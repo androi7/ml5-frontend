4110e582f859f2dfe6b367d322f60a43602558e0
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-03-05 15:10:46 +1100

    gallery css

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,7 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import ajax from '../../lib/ajax';
+import { Grid, Paper } from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { makeStyles } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const styles = {
+  image: {
+    width: '100%',
+    marginTop: '5vh',
+    borderRadius: 5,
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);',
+    '&:hover': {
+      opacity: 0.5
+    }
+  }
+};
+
+const useStyles = makeStyles(styles);
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiSkeleton: {
+      root: {
+        display: 'inline-block',
+        margin: 10,
+        backgroundColor: '#00000033'
+      }
+    }
+  }
+});
 
 const Gallery = () => {
+
+  const classes = useStyles();
 
   const [token, setToken] = useState('');
   const [gallery, setGallery] = useState([]);
@@ -9,18 +41,42 @@ const Gallery = () => {
   useEffect(() => {
     const token = localStorage.getItem('faceapi-token');
     setToken(token);
+
   }, []);
 
 
   useEffect(() => {
     ajax.getGallery(token).then(res => {
-      setGallery(res.data.map((img, ind) => <img key={ind} src={img} width="200" alt="" />));
+      setGallery(res.data.map((img, ind) => (
+        <Grid item xs={4} md={3}>
+          <img key={ind} src={img} className={classes.image} alt="" />
+        </Grid>
+      )
+    ));
     });
   }, [token]);
 
+
   return (
     <div>
-      {gallery}
+      <Grid container spacing={5} justify="center">
+      {
+        gallery.length > 0
+        ? gallery
+        : <MuiThemeProvider theme={theme}>
+            <Fragment>
+              <div style={{textAlign: 'center', marginTop: 50}}>
+              <Skeleton variant="rect" width={300} height={200} />
+              <Skeleton variant="rect" width={300} height={200} />
+              </div>
+              <div style={{textAlign: 'center', margin: 'auto'}}>
+              <Skeleton variant="rect" width={300} height={200} />
+              <Skeleton variant="rect" width={300} height={200} />
+              </div>
+            </Fragment>
+          </MuiThemeProvider>
+      }
+      </Grid>
     </div>
   );
 };
